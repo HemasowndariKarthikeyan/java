604b82614b6cab302c22aad71c8dd88b56fb735b
rajsa <rajsa@LAPTOP-AG3RTCVE>
2023-04-25 22:13:31 -0700

    first commit

---
+++ c2tc/src/day1/intro.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class intro {
+       public static void main(String[]args) {
+    	   System.out.println("welcome sir");
+    	   System.out.println("Nice to meet you sir!");
+    	   System.out.println("intro.main()");
+    	   
+       }
+       
+}
