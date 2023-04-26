b2f7c43a26b5c9560a6c9257f04dce44c0ca7cdb
HemasowndariKarthikeyan <129287608+HemasowndariKarthikeyan@users.noreply.github.com>
2023-04-25 20:18:32 -0700

    Add files via upload

---
+++ day3/encapsdemo.java
@@ -0,0 +1,11 @@
+package day3;
+
+public class EncapsDemo {
+	
+ public static void main(String[] args) {
+	 Encaps e = new Encaps();
+	 e.setId(1507);
+	 e.setName("Hema sowndari");
+	 System.out.println(o);
+ }
+}
