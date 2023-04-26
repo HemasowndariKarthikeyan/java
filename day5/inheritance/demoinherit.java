2585191e4a2bd561fdc20f55b534f36bd8ea07f7
HemasowndariKarthikeyan <129287608+HemasowndariKarthikeyan@users.noreply.github.com>
2023-04-25 20:36:08 -0700

    Add files via upload

---
+++ day5/inheritance/demoinherit.java
@@ -0,0 +1,10 @@
+package day5.Inheritance;
+
+public class Demoinhert {
+	
+	public static void main(String[] args) {
+		Student stud = new Student("Hema", "Chennai",6383149390, 20,"DSC");
+		System.out.println(stud);
+	}
+
+}
